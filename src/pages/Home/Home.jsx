7252723f70a3df4cf0df602615d3bb2d75a07bb0
gh3ma <gaes.sy.2000@gmail.com>
2025-05-19 13:14:50 +0300

    1

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState ,useEffect } from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
+const Home = () => {
+
+  const [category,setCategory] = useState("All")
+useEffect(() => {
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script");
+      var s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/682a166ae37b45190d4a0254/1iri6f4pi';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
+
+
+
+
+  return (
+    <>
+      <Header/>
+      <ExploreMenu setCategory={setCategory} category={category}/>
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+      
+    </>
+  )
+}
+
+export default Home
